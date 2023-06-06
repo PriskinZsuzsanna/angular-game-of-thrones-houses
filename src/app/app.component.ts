de67b476f6d4b71c9ec3ddde42ae39ab8d8af291
Priskin Zsuzsanna <priskin.zsuzsanna@gmail.com>
2023-06-06 14:28:32 +0200

    ready3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { faDungeon } from '@fortawesome/free-solid-svg-icons';
+import { faMountainSun } from '@fortawesome/free-solid-svg-icons';
+import { faShield } from '@fortawesome/free-solid-svg-icons';
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -20,7 +29,17 @@ export class AppComponent {
   selected: boolean = false
   fadeAway: boolean = false
 
-  constructor(private _http: HttpClient){
+  faDungeon = faDungeon
+  faMountainSun = faMountainSun
+  faShield = faShield
+  faQuoteRight = faQuoteRight
+  faQuoteLeft = faQuoteLeft
+  faSun = faSun
+  faMoon = faMoon
+
+  light: boolean = false
+
+  constructor(@Inject(DOCUMENT) private document: Document ,private _http: HttpClient){
     
   }
 
@@ -50,5 +69,10 @@ export class AppComponent {
       this.coatOfArms = this.selectedHouse.coatOfArms
       this.words = this.selectedHouse.words
     }, 1000)
+  }
+
+  toggleTheme(){
+    this.light = !this.light
+    this.document.body.classList.toggle("light");
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ButtonComponent } from './button/button.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
